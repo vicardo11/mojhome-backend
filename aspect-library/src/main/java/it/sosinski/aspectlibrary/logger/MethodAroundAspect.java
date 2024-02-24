c1a230372356d4f813b77afcf2cb7ab78198a646
Patryk Sosinski <patryk.sosinski@lhsystems.com>
2024-02-24 15:54:56 +0100

    Introduce aspect to log entry and exit of method

---
+++ aspect-library/src/main/java/it/sosinski/aspectlibrary/logger/MethodAroundAspect.java
@@ -0,0 +1,66 @@
+package it.sosinski.aspectlibrary.logger;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MethodAroundAspect {
+
+	/***
+	 * Logging method entry in format: methodName(parameters)
+	 * And method exit in format: methodName()=result
+	 *
+	 * For readability, it's good to implement toString() method in all the classes which can be used as method params.
+	 */
+	@Around("@annotation(it.sosinski.aspectlibrary.logger.LogMethodAround)")
+	public Object logMethodAround(final ProceedingJoinPoint joinPoint) throws Throwable {
+		final String className = getFullClassName(joinPoint);
+		final String methodName = getMethodName(joinPoint);
+		final List<String> parameters = mapArgumentsToStringList(joinPoint.getArgs());
+
+		final Logger logger = Logger.getLogger(className);
+		logger.fine(formatEnterMessage(methodName, parameters));
+
+		final Object proceed = joinPoint.proceed();
+
+		logger.fine(formatExitMessage(methodName, proceed));
+
+		return proceed;
+	}
+
+	private String getFullClassName(final ProceedingJoinPoint joinPoint) {
+		return joinPoint.getTarget()
+				.getClass()
+				.getName();
+	}
+
+	private String getMethodName(final JoinPoint joinPoint) {
+		return joinPoint.getSignature()
+				.getName();
+	}
+
+	private List<String> mapArgumentsToStringList(final Object[] args) {
+		return Arrays.stream(args)
+				.map(Object::toString)
+				.collect(Collectors.toList());
+	}
+
+	private String formatEnterMessage(final String methodName, final List<String> params) {
+		final String collectedParams = String.join(", ", params);
+		return String.format("%s(%s)", methodName, collectedParams);
+	}
+
+	private String formatExitMessage(final String methodName, final Object result) {
+		final String resultAsString = result.toString();
+		return String.format("%s()=%s", methodName, resultAsString);
+	}
+}
