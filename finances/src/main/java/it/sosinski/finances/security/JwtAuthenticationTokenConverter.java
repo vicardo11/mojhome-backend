aafbe5cbc29427e6c5de73c4bdf32ac1c566c24f
Patryk Sosinski <patryk.sosinski@lhsystems.com>
2024-02-25 10:29:55 +0100

    Basic security configuration

---
+++ finances/src/main/java/it/sosinski/finances/security/JwtAuthenticationTokenConverter.java
@@ -0,0 +1,37 @@
+package it.sosinski.finances.security;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationTokenConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+	@Override
+	public AbstractAuthenticationToken convert(@NonNull final Jwt jwt) {
+		final Collection<GrantedAuthority> authorities = new HashSet<>(extractResourceRoles(jwt));
+		return new JwtAuthenticationToken(jwt, authorities, jwt.getClaim(JwtClaimNames.SUB));
+	}
+
+	private Collection<? extends GrantedAuthority> extractResourceRoles(final Jwt jwt) {
+		final List<String> resourceAccess = jwt.getClaim("groups");
+		if (resourceAccess.isEmpty()) {
+			return Collections.emptySet();
+		}
+		return resourceAccess.stream()
+				.map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+				.collect(Collectors.toSet());
+	}
+}
---
+++ finances/src/main/java/it/sosinski/finances/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package it.sosinski.finances.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final JwtAuthenticationTokenConverter jwtAuthenticationTokenConverter;
+
+	@Bean
+	public SecurityFilterChain resourceServerFilterChain(final HttpSecurity http) throws Exception {
+		return http
+				.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+						.anyRequest().hasRole("user"))
+				.oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(
+						jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationTokenConverter)))
+				.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.cors(Customizer.withDefaults())
+				.build();
+	}
+
+}
